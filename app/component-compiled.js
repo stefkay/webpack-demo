43ed20e7e8071460081cfbb6d8eb0812eade4148
Стефка Кайкева < >
2016-07-13 17:38:44 +0300

    Updates

---
+++ app/component-compiled.js
@@ -0,0 +1,9 @@
+module.exports = function () {
+	var styles = require('./main.css');
+	var element = document.createElement('h1');
+	element.className = 'pure-button';
+	element.innerHTML = "Hello world";
+	return element;
+};
+
+//# sourceMappingURL=component-compiled.js.map
---
+++ build/app.79f021b93e9d50913240-compiled.js
@@ -0,0 +1,10 @@
+webpackJsonp([0, 3], { 0: function (n, e, o) {
+    o(1);var t = o(29);document.body.appendChild(t());
+  }, 29: function (n, e, o) {
+    n.exports = function () {
+      var n = (o(30), document.createElement("h1"));return n.className = "pure-button", n.innerHTML = "Hello world", n;
+    };
+  } });
+//# sourceMappingURL=app.79f021b93e9d50913240.js.map
+
+//# sourceMappingURL=app.79f021b93e9d50913240-compiled.js.map
